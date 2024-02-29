74dc7ffff733a70e74c22f13f0c4c5064bec82e8
Paula Kukuczka <kukupaulina@gmail.com>
2024-02-29 15:58:07 +0100

    new JS hamburger nav

--- js/navigation.js
+++ js/navigation.js
@@ -1,24 +1,54 @@
+// function myFunction() {
+//     let clickOnBtn = document.querySelectorAll('.menu__item');
+//     let x = document.querySelector('.menu__item-list');
+//     if (x.style.display === 'block') {
+//         x.style.display = 'none';
+//     } else {
+//         x.style.display = 'block';
+//     }
 
-function myFunction() {
-    let clickOnBtn = document.querySelectorAll('.menu__item');
-    let x = document.querySelector('.menu__item-list');
-    if (x.style.display === 'block') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
+//     //obsługa kliknięcia myszy
+//     clickOnBtn.forEach(item => {
+//         item.addEventListener('click', () => {
+//             x.style.display = 'none';
+//         })
+//     })
 
-    //obsługa kliknięcia myszy
-    clickOnBtn.forEach(item => {
-        item.addEventListener('click', () => {
-            x.style.display = 'none';
-        })
-    })
+//     //obsługa dotyku na urządzeniach mobilnych
+//     clickOnBtn.forEach(item => {
+//         item.addEventListener('touchstart', () => {
+//             x.style.display = 'none';
+//         });
+//     });
+// // do HTML
+// // onclick="myFunction()"
+// // 					ontouchstart="myFunction()"
+// }
 
-    //obsługa dotyku na urządzeniach mobilnych
-    clickOnBtn.forEach(item => {
-        item.addEventListener('touchstart', () => {
-            x.style.display = 'none';
-        });
-    });
-}
+const nav = document.querySelector('.menu');
+const hamburgerBtn = document.querySelector('.hamburger-icon');
+const navItemList = document.querySelector('.menu__item-list');
+const navItem = document.querySelectorAll('.menu__link')
+
+const handleNav = () => {
+	// nav.classList.toggle('menu--active');
+	// console.log(nav);
+
+	// hamburgerBtn.style.display = 'block';
+
+	hamburgerBtn.addEventListener('click', () => {
+		if (navItemList.style.display === 'block') {
+			navItemList.style.display = 'none';
+		} else {
+			navItemList.style.display = 'block';
+
+			navItem.forEach((item) => {
+				item.addEventListener('click', () => {
+					navItemList.style.display = 'none';
+				})
+			})
+		}
+	});
+};
+
+handleNav();
